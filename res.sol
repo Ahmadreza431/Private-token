390837bb68dc037a5158888912930712dae1018f
"Ahmadreza431" <"ahmadreza.maleki0@gmail.com">
2022-03-17 03:36:18 +0330

    Dev 31722

--- res.sol
+++ res.sol
@@ -6,20 +6,20 @@ contract Restaurant{
 
     address public owneraddress;
     address public contrataddress;
-    uint32 public tp; //tottall price
+    uint public tp; //tottall price
      
      struct foodmenu{
-         uint32 id;
-         uint8 price;
+         uint id;
+         uint price;
          string name;
      }
 
      foodmenu[] Foodmenu;
 
      struct orderlist{
-         uint32 id;
-         uint8 Number;
-         uint32 tprice;
+         uint id;
+         uint Number;
+         uint tprice;
          string name;
          string delivery;
          bool Expiredorder;
@@ -48,18 +48,23 @@ contract Restaurant{
         _;
      }
 
-     function RegisterMenu(uint32 id , uint8 price , string memory name) public Owner(){
-         uint8 i;
-         for (i=0;i<Foodmenu.length;i++){
-             require(Foodmenu[i].id ==id,"The Idcode entered is duplicate...");
+     function RegisterMenu(uint id , uint price , string memory name) public Owner(){
+         if (Foodmenu.length==0){
              Foodmenu.push(foodmenu(id,price,name));
              fazes = FAZ.setorder;
+         } else{        
+             uint i;
+             for (i=0;i<=Foodmenu.length;i++){
+                 require(Foodmenu[i].id == id,"The IDcode entered is duplicate...");
+                 Foodmenu.push(foodmenu(id,price,name));
+                 fazes = FAZ.setorder;
+                }
             }
-         }
+        }
 
-     function getorder(uint32 id , uint8 num ,string memory name , string memory delivery) public payable{
+     function getorder(uint id , uint num ,string memory name , string memory delivery) public payable{
          tp=0;
-         uint8 i;
+         uint i;
          for (i=0;i<Foodmenu.length;i++){
              if (Foodmenu[i].id == id){
                  tp=Foodmenu[i].price*num;
@@ -70,11 +75,12 @@ contract Restaurant{
          require(tp > 0 ,"The desired food was not found");
          //require(msg.value > tp ,"Your account balance is not enough");
          Orderlist.push(orderlist(id,num,tp,name,delivery,false,msg.sender));
-
+         fazes = FAZ.setpay;
      }
 
      function payment() public payable{
-         uint8 i;
+         require(fazes == FAZ.setpay,"There is nothing to pay...");
+         uint i;
          tp=0;
          for (i=0;i<Orderlist.length;i++){
               if (msg.sender == Orderlist[i].Customeraddress){
